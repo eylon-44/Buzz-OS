0b5f10e1558149e6e9f7df2577f9743532bdaf58
Eylon <eylon462@gmail.com>
2024-06-19 08:49:14 +0300

    User makefile bug fix

--- usr/exe/terminal/src/main.c
+++ usr/exe/terminal/src/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main()
 {
-    printf("Hey there!");
-    printf("I am a program running on Buzz. Come join!");
+    char* input = malloc(128);
+
+    printf("Enter your name: ");
+    input[syscall(SYS_read, stdin, input, 128)] = '\0';
+
+    printf(" Hello %s", input);
 
     while (1) {}
 }
